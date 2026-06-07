ba11f6da5804df4b70959115ab2bb660dff0e947
Muhammad Ahmed <aq320646@gmail.com>
2026-06-07 05:42:00 +0500

    update smooth scroll

--- 01_personal_portfolio_website/components/SmoothScroll.jsx
+++ 01_personal_portfolio_website/components/SmoothScroll.jsx
@@ -5,7 +5,7 @@ import Lenis from "lenis";
 export default function SmoothScroll({ children }) {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 1.5,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smooth: true,
       mouseMultiplier: 1,
